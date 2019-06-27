c04de0893143e4f1cbeec5fe5c53775e4c8fe3c3
Tom Walker <Tom@Toms-Mac-mini.local>
2019-06-27 11:31:48 +0100

    Update styles and animation

--- src/javascripts/modules/button.js
+++ src/javascripts/modules/button.js
@@ -2,14 +2,19 @@ import { utils } from './utils';
 import 'gsap';
 import '../vendor/DrawSVGPlugin';
 
+const letters = Array.from(document.querySelectorAll('.js-letter'));
 const loadBtn = document.querySelector('.js-btn-load');
 const loadBtnContainer = document.querySelector('.js-btn-load-container');
 const circle = document.querySelector('#load-circle');
 const tick = document.querySelector('#load-tick');
 
+const lettersTl = new TimelineLite({paused: true});
+lettersTl.staggerTo(letters.reverse(), 0.2, {y: 20, opacity: 0, ease: Back.easeIn.config(1.2)}, 0.075);
+
 let done;
 
 const circleTl = new TimelineMax({
+  delay: 0.3,
   paused: true,
   repeat: -1,
   onStart: function() {
@@ -43,26 +48,20 @@ tickTl
   ease: Power1.easeOut,
   onComplete: function() {
     TweenLite.delayedCall(0.5, function() {
-      widthTl.reverse();
+      fadeTl.reverse();
+      lettersTl.reverse();
     });
   }
 })
 
-const widthTl = new TimelineLite({
-  paused: true
-})
-
-widthTl
-.from(loadBtnContainer, 0.2, {
-  opacity: 0,
-  width: 0,
-  marginLeft: 0
-})
+const fadeTl = new TimelineLite({paused: true, delay: 0.3});
+fadeTl.fromTo(loadBtnContainer, 0.2, {opacity: 0}, {opacity: 1});
 
 const loadBtnAnim = function() {
   TweenLite.set(tick, {drawSVG: "100% 100%"});
-  widthTl.restart();
-  circleTl.restart();
+  lettersTl.restart();
+  fadeTl.restart(true);
+  circleTl.restart(true);
 }
 
 const button = {
@@ -73,8 +72,16 @@ const button = {
       // Simulate AJAX call..
       setTimeout(function() {
         done = true;
-      }, 1000)
+      }, 2500)
     });
+
+    loadBtn.addEventListener('mousedown', function(e) {
+      TweenLite.to(loadBtn, 0.01, {scale: 0.98});
+    })
+
+    loadBtn.addEventListener('mouseup', function(e) {
+      TweenLite.to(loadBtn, 0.01, {scale: 1});
+    })
   }
 }
 
